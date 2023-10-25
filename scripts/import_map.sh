bd07efb616f5d4db75c7b2808a673eca8e501ed1
yangzhanxi <yangzhanxi@163.com>
2023-10-25 09:38:16 +0800

    Added scripts

---
+++ scripts/import_map.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+echo "log 1"
+sleep 2
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
+
+echo "log 1"
+sleep 2
