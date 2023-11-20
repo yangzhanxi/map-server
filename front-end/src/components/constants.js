0b56268bbad951eab22dcdc5b0bdd9f3eb8195fb
yangzhanxi <yangzhanxi@163.com>
2023-11-20 02:28:52 +0000

    added https supports

--- front-end/src/components/constants.js
+++ front-end/src/components/constants.js
@@ -1,6 +1,6 @@
 const HOST = window.location.hostname
 const PORT = 58885
-const URL = `http://${HOST}:${PORT}`
+const URL = `https://${HOST}:${PORT}`
 const IMPORT_EVENTS = `${URL}/import-map-events`
 const UPLOAD_FILE = `${URL}/import-map`
 
